c40b476fdc69e9d72777be58e57a0fc467a07613
Yevhenii Zhyrov <yevheniizhyrov@gmail.com>
2023-05-14 23:08:52 +0300

    Init nextjs + storybook + chromatic + tailwindcss

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+const config: StorybookConfig = {
+  stories: [
+    '../pages/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    // '../react-bricks/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
