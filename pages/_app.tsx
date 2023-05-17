e060cc211977b6d834477162aef21d2a64323ceb
Yevhenii Zhyrov <yevheniizhyrov@gmail.com>
2023-05-17 21:59:45 +0300

    init tinacms

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,9 @@
 import "../css/global.css"
 
-import { Accordion, AccordionContainer } from "../components/accordion"
+// import { Accordion, AccordionContainer } from "../components/accordion"
 
-const MyApp = () => {
-  return (
-    <AccordionContainer>
-      <Accordion title={"Foo"}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, temporibus! Delectus eos quos tenetur error.
-      </Accordion>
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-      <Accordion title={"Bar"}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, temporibus! Delectus eos quos tenetur error.
-      </Accordion>
-
-      <Accordion title={"Baz"}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, temporibus! Delectus eos quos tenetur error.
-      </Accordion>
-    </AccordionContainer>
-  )
-}
-
-export default MyApp
+export default App
---
+++ tina/config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "84efb77b-40fc-4f63-908a-2642293e07a5", // Get this from tina.io
+  token: "9c893003ac24a50ecbfeff6df59e842ee888bd6f", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
